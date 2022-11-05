da667ee712737ea9d416a42719bdbda82e3c4415
KyeongSeopHam <rudtjq5724@gmail.com>
2022-11-05 17:20:54 +0900

    초기설정grade

---
+++ src/test/java/com/example/dividendsproject/DividendsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dividendsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
